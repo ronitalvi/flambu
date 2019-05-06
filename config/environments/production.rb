51c9fb5ec0a0ebd29c9ce41fc19ba88753ecd367
roni talvi <vtalvi@gmail.com>
2019-05-06 13:04:35 +0300

    redis added for action cable on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://www.flambuapp.herokuapp.top/cable"
+  config.action_cable.url = "ws://www.flambuapp.herokuapp.top/cable"
   config.action_cable.allowed_request_origins = [ "http://www.flambuapp.herokuapp.top", "https://www.flambuapp.herokuapp.top" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
