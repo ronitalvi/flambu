887d478eecc0bc5c09c3df09b0e3fd8627587ba7
roni talvi <vtalvi@gmail.com>
2019-05-06 13:27:56 +0300

    redis added for action cable on production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-uri = URI.parse(ENV["REDISCLOUD_URL"])
+uri = URI.parse("redis://rediscloud:hBkXgqI1QbcuC9780zoyW9IqN78nr9II@redis-18440.c59.eu-west-1-2.ec2.cloud.redislabs.com:18440")
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+
+##burda sorun var
